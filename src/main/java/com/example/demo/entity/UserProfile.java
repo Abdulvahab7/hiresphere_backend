2ec57200da5c80983a62adb91db86b81f9119c1b
Abdulvahab7 <abdulawm407@gmail.com>
2026-07-08 10:24:17 +0530

    Add JsonIgnoreProperties annotation to UserProfile

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
